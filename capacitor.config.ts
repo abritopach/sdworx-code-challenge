93c0f2926b28862c3d204074366e0da1067d20ff
Adrian Brito <adrianbrito@Adrians-MBP.netis.cc>
2023-10-02 21:28:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sdworx-code-challenge',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
